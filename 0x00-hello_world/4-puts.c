f6cb590403134f2e140f3a5082a7ab4a98e5d37a
Hibak10 <abdullahihibak36@gmail.com>
2023-09-07 14:11:19 -0700

    intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
-   *main - prints "Programming is like building
-   *       a multilingual puzzle, followed by a new line.
+i/*
+   *main -  use of put function.  
    *Author:Hibak Abdy.
-   *return: Always (0) Success
+   *return: Always (0) Success.
    */
 int main(void)
 {
