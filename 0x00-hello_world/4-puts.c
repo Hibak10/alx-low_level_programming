a36088073236a3dee7c78f59b499378827c8a9a9
Hibak10 <abdullahihibak36@gmail.com>
2023-09-07 12:43:47 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+   *main - prints "Programming is like building
+   *       a multilingual puzzle, followed by a new line.
+   *Author:Hibak Abdy.
+   *return: Always (0) Success
+   */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
