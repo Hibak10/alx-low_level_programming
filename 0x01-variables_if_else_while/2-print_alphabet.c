f3478bb0fdc7ab2bac68772b43ba4f969ceebc0d
Hibak10 <abdullahihibak36@gmail.com>
2023-09-08 15:43:15 -0700

    last_digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main to print alphabets in lower case
+  *description: print alphabets in lower case a-z
+  *return always (0)
+  */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z');
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
