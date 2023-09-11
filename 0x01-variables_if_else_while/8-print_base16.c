434be165e8d390034e26cc291c3d77525d642655
Hibak10 <abdullahihibak36@gmail.com>
2023-09-11 10:36:18 -0700

    base16.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - use putchar to print out all numbers of base 16
+  *Description: prints all the numbers of base 16 in lowercase
+  * only use the putchar function
+  *use putchar three times in the code
+  *return: (0)
+  */
+int main(void)
+{
+	int num;
+	char alphabet;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+	return (0);
+}
