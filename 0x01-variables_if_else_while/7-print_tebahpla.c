59fa468464adb879789ba6700bc40d513392fea8
Hibak10 <abdullahihibak36@gmail.com>
2023-09-11 11:50:18 -0700

    7-print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,9 +7,9 @@
   */
 int main(void)
 {
-	char alphabet
+	char alphabet;
 
-		for (alphabet = 'z'; alphabet <= 'a'; alphabet++)
+		for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
 			putchar(alphabet);
 
 	putchar('\n');
