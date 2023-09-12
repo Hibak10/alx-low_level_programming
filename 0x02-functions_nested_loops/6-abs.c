571dea70ac14c2eb1a845e4f0022589d3f65353f
Hibak10 <abdullahihibak36@gmail.com>
2023-09-12 11:48:20 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_abs -  to return the absolute interger
+  *@n: the number to be returned
+  *
+  *Return: absolute value of the interger
+  *
+  */
+int _abs(int n)
+{
+
+	if (n >= 0)
+		return (n);
+	else
+		return (0);
+}
