77e345339a871e34678994e9b3ed5928037ceffa
Hibak10 <abdullahihibak36@gmail.com>
2023-09-13 06:22:51 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main - generate random numbers to test -ve or +ve
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
  *
- * return always (0)
- *
+ * Return: Always 0.
  */
 int main(void)
 {
