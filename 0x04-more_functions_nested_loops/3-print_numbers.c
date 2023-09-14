4d348c8fe6f45fcfe4149dcdeb4cbdf52c9c8a50
Hibak10 <abdullahihibak36@gmail.com>
2023-09-14 12:31:39 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *
+  *
+  *
+  *
+  *
+  */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	if ((i = 48) && (i < 58))
+		i++;
+
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
+
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_most_numbers - prints numbers except 2 and 4
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
+{
+	int digit;
+
+	for (digit = 48; digit < 58; digit++)
+	{
+		if ((digit == 50) || (digit == 52))
+		{
+			continue;
+		}
+	}
+	_putchar(digit);
+	_putchar('\n');
+}
+
