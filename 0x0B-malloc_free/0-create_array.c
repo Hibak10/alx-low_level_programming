38ef356bffadfd74f5ac86065874a3ab26e6e9e1
Hibak10 <abdullahihibak36@gmail.com>
2024-01-20 04:51:12 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return( NULL);
+		return (NULL);
 	}
 
 	array = (char *)malloc(size * sizeof(char));
@@ -31,7 +31,6 @@ char *create_array(unsigned int size, char c)
 
 	for (i = 0; i <= size; i++)
 		array[i] = c;
-	
 	return (array);
 }
 
