941a905603b0898c6d7ee554d79ed7ca03223bdb
Hibak10 <abdullahihibak36@gmail.com>
2024-01-21 03:42:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
-   *main - Entry point.
-   *Author:Hibak Abdy.
-   *return: Always (0) Success.
+   *main - Entry point
+   *
+   *
+   *return: Return 0 everytime
    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
