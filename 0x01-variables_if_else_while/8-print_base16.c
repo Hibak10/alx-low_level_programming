c028dd72f1148c28a4567402604c25a137faaee0
Hibak10 <abdullahihibak36@gmail.com>
2024-03-24 23:21:07 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 
 /**
@@ -12,8 +11,8 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	for (num = '0'; num < '9'; num++)
+		putchar(num);
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
