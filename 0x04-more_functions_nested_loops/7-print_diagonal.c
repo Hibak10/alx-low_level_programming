0e85307c898ea30bb2f5a1d066b24b3f18a5a87e
Hibak10 <abdullahihibak36@gmail.com>
2024-04-15 13:33:47 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,14 +20,12 @@ void print_diagonal(int n)
 		{
 
 			for (j = 0; j <= i; j++)
-
 				_putchar(' ');
-		_putchar('\\');
-
-		_putchar('\n');
+			_putchar('\\');
+			_putchar('\n');
 		}
-	}
 
+	}
 }
 
 
