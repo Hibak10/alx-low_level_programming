42404170539548dd64d221f9bbec1385d6288acc
Hibak10 <abdullahihibak36@gmail.com>
2024-06-27 14:46:16 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ *main - Prints all the arguments it receives including with
+ *	the first one, each per line.
+ *@argc: Argument count
+ *@argv: Array name
+ *Return:0
+ */
 
 int main(int argc, char *argv[])
 {
