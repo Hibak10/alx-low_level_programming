43fadcb21734f9170a5093d5db73b7cf8c0f3340
Hibak10 <abdullahihibak36@gmail.com>
2024-08-28 13:47:30 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,5 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 int _strlen(char *s);
-char *_strdpy(char *dest, char *s);
-#endifi
+char *_strcpy(char *dest, char *s);
+#endif
