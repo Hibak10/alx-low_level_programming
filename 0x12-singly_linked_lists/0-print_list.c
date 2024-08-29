201f953cbe6ecb665a5ebf626d0b5c406481bc82
Hibak10 <abdullahihibak36@gmail.com>
2024-08-29 13:46:06 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *print_list -  Funtion that printts all elements of list_t list
+ *Return: The total number of nodes
+ *@h: A pointer to the head node of alinked list
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u][%s]\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
